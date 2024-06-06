55bb6c073079426217b730c6fe91036b897017ea
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-06-06 17:34:36 -0300

    Create TaskMap.cs

---
+++ Datas/Maps/TaskMap.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskSystemApi.Models;
+
+namespace TaskSystemApi.Data.Map
+{
+
+    public class TaskMap : IEntityTypeConfiguration<TaskModel>
+    {
+
+        public void Configure(EntityTypeBuilder<TaskModel> builder)
+        {
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Description).HasMaxLength(1000);
+            builder.Property(x => x.Status).IsRequired();
+            builder.Property(x => x.UserId);
+
+            builder.HasOne(x => x.User);
+
+        }
+
+    }
+
+}
