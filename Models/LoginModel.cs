ebbd63f542c017f6c9da35a8b3fe263b2bdf6879
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-06-06 17:42:58 -0300

    Create LoginModel.cs

---
+++ Models/LoginModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskSystemApi.Models
+{
+
+    public class LoginModel
+    {
+
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+
+    }
+
+}
