59eeb808ca0f1b962832bbb9e3d728a8264fed23
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-06-06 17:43:29 -0300

    Create UserModel.cs

---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskSystemApi.Models
+{
+
+    public class UserModel
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+
+}
