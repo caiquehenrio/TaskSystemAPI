add6bc8b72d9ca81505a5177f58b63259ecf29ca
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-06-06 17:34:53 -0300

    Create UserMap.cs

---
+++ Datas/Maps/UserMap.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskSystemApi.Models;
+
+namespace TaskSystemApi.Data.Map
+{
+
+    public class UserMap : IEntityTypeConfiguration<UserModel>
+    {
+
+        public void Configure(EntityTypeBuilder<UserModel> builder) 
+        {
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(150);
+
+        }
+
+    }
+
+}
